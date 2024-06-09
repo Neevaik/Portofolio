1e7dac8cd33e4a8be40c370560124393529513cc
Kevin <kevin.mavier@hotmail.com>
2024-06-10 01:21:45 +0200

    init next project

---
+++ components/navbar.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/about">About</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
