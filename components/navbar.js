b7a757b94ca2a0e16e5026f3c06da14a7fc13f20
Kevin <kevin.mavier@hotmail.com>
2024-06-12 01:45:18 +0200

    front page title

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Link from 'next/link';
 
-export default function Navbar() {
+export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -9,33 +9,39 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link href="/"className="text-xl font-bold">Kev.</Link>
-        <div className="hidden md:flex space-x-4">
-          <Link href="/">Accueil</Link>
-          <Link href="/projects">Projets</Link>
-          <Link href="/about">À propos</Link>
-          <Link href="/contact">Contact</Link>
-        </div>
+    <>
+      <nav className="p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <Link href="/" className="text-4xl font-bold ml-20">
+            <span>Kevin</span>
+            <span className="text-red-500">.</span>
+          </Link>
+          <div className="hidden md:flex space-x-8 mr-100">
+            <Link href="/" className="hover:text-black hover:bg-white p-2 rounded">Accueil</Link>
+            <Link href="/projects" className="hover:text-black hover:bg-white p-2 rounded">Projets</Link>
+            <Link href="/about" className="hover:text-black hover:bg-white p-2 rounded">Skills</Link>
+            <Link href="/contact" className="hover:text-black hover:bg-white p-2 rounded">Contact</Link>
+          </div>
 
-        <div className="md:hidden">
-          <button
-            className="text-white focus:outline-none"
-            onClick={toggleMenu}
-          >
-            Menu
-          </button>
+          <div className="md:hidden">
+            <button
+              className="text-white focus:outline-none"
+              onClick={toggleMenu}
+            >
+              Menu
+            </button>
+          </div>
         </div>
-      </div>
-      {isOpen && (
-         <div className="md:hidden mt-2 flex flex-col space-y-2 p-4 rounded-lg">
-          <Link href="/" className="py-2 border-b border-gray-600">Accueil</Link>
-          <Link href="/projects" className="py-2 border-b border-gray-600">Projets</Link>
-          <Link href="/about" className="py-2 border-b border-gray-600">À propos</Link>
-          <Link href="/contact" className="py-2">Contact</Link>
-        </div>
-      )}
-    </nav>
+        {isOpen && (
+          <div className="md:hidden mt-2 flex flex-col space-y-2 p-4 rounded-lg">
+            <Link href="/" className="py-2 border-b border-gray-600 hover:text-black hover:bg-white p-2 rounded">Accueil</Link>
+            <Link href="/projects" className="py-2 border-b border-gray-600 hover:text-black hover:bg-white p-2 rounded">Projets</Link>
+            <Link href="/about" className="py-2 border-b border-gray-600 hover:text-black hover:bg-white p-2 rounded">À propos</Link>
+            <Link href="/contact" className="py-2 hover:text-black hover:bg-white p-2 rounded">Contact</Link>
+          </div>
+        )}
+      </nav>
+
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen flex items-center justify-center text-white px-4">
+      <div className="max-w-screen-lg flex">
+        <div className="flex flex-col justify-center">
+          <h2 className="text-xl mb-4">Software Developer</h2>
+          <h1 className="text-5xl font-bold mb-6">Hello I am</h1>
+          <h1 className="text-7xl font-bold mb-6">
+            <span className="bg-gradient-to-r text-transparent from-pink-500 to-purple-500 bg-clip-text">Kevin Mavier</span>
+          </h1>
+          <p className="text-xl mb-8">Welcome to my portfolio. Here you'll find my projects and more about my skills.</p>
+        </div>
+        <img src="https://images.pexels.com/photos/670720/pexels-photo-670720.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Profile picture" className="flex-grow h-auto w-64" />
+      </div>
     </div>
   );
 }
