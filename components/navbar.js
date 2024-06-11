33d143ae77d87b949fd45f57dc171d62477d2d47
Kevin <kevin.mavier@hotmail.com>
2024-06-11 12:45:49 +0200

    set bg & navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,22 +1,41 @@
+import { useState } from 'react';
 import Link from 'next/link';
-import styles from '../styles/Navbar.module.css';
 
-const Navbar = () => {
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.navList}>
-        <li className={styles.navItem}>
-          <Link href="/">Home</Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/about">About</Link>
-        </li>
-        <li className={styles.navItem}>
+    <nav className="p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/"className="text-xl font-bold">Kev.</Link>
+        <div className="hidden md:flex space-x-4">
+          <Link href="/">Accueil</Link>
+          <Link href="/projects">Projets</Link>
+          <Link href="/about">À propos</Link>
           <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
+        </div>
+
+        <div className="md:hidden">
+          <button
+            className="text-white focus:outline-none"
+            onClick={toggleMenu}
+          >
+            Menu
+          </button>
+        </div>
+      </div>
+      {isOpen && (
+         <div className="md:hidden mt-2 flex flex-col space-y-2 p-4 rounded-lg">
+          <Link href="/" className="py-2 border-b border-gray-600">Accueil</Link>
+          <Link href="/projects" className="py-2 border-b border-gray-600">Projets</Link>
+          <Link href="/about" className="py-2 border-b border-gray-600">À propos</Link>
+          <Link href="/contact" className="py-2">Contact</Link>
+        </div>
+      )}
     </nav>
   );
-};
-
-export default Navbar;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
@@ -7,8 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
-      
-    </>
+    <div>
+    </div>
   );
 }
